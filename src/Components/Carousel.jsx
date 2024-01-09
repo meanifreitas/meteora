968c8c6cdf569bbb78e071664705d2f73a67be69
Meani Freitas <meani.sf@gmail.com>
2024-01-09 14:43:52 -0300

    Carousel component

--- src/Components/Carousel.jsx
+++ src/Components/Carousel.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import useDeviceType from '../Hooks/useDevideType';
 
+import styles from './Carousel.module.css';
+
 import Slide1Desktop from '../assets/Desktop/Banner-carousel-1.png';
 import Slide1Mobile from '../assets/Mobile/Banner-carousel-1.png';
 import Slide1Tablet from '../assets/Tablet/Banner-carousel-1.png';
@@ -13,9 +15,20 @@ import Slide3Desktop from '../assets/Desktop/Banner-carousel-3.png';
 import Slide3Mobile from '../assets/Mobile/Banner-carousel-3.png';
 import Slide3Tablet from '../assets/Tablet/Banner-carousel-3.png';
 
+import { ArrowLeft, ArrowRight } from 'phosphor-react';
+
 
 const Carousel = () => {
   const deviceType = useDeviceType();
+  const [position, setPosition] = React.useState(0);
+  const [active, setActive] = React.useState(0);
+  const contentRef = React.useRef();
+
+  React.useEffect(() => {
+    const { width } = contentRef.current.getBoundingClientRect();
+    setPosition(-(width*active));
+
+  }, [active])
   
   const slides = [{
     id: 1,
@@ -33,19 +46,44 @@ const Carousel = () => {
     subtitle: 'High visual impact, low environmental impact!'
   }];
 
+  function prevSlide() {
+    if (active > 0) setActive(active - 1);
+    else setActive(slides.length - 1);
+  }
+
+  function nextSlide() {
+    if (active < slides.length - 1) setActive(active + 1);
+    else setActive(0);
+  }
+
+  function handleClick(slide) {
+    setActive(slide);
+  }
+
   return (
-    <section>
-      <div>
+    <section className={styles.container}>
+      <div className={styles.content} ref={contentRef} style={{transform: `translateX(${position}px)`}}>
         {slides.map(slide => {
           return (
-            <div key={slide.id}>
+            <div key={slide.id} className={styles.item}>
               <img src={slide.img}/>
-              <h3>{slide.title}</h3>
-              <p>{slide.subtitle}</p>
+              <div className={`${styles.description} ${slide.id === 2 ? styles.second : styles.third}`}>
+                <h3>{slide.title}</h3>
+                <p>{slide.subtitle}</p>
+              </div>
             </div>
           )
         })}
       </div>
+      <nav className={styles.arrowNav}>
+        <button onClick={prevSlide}><ArrowLeft size={20} /></button>
+        <button onClick={nextSlide}><ArrowRight size={20} /></button>
+      </nav>
+      <nav className={styles.buttonNav}>
+        {slides.map(slide => {
+          return <button key={slide.id} onClick={() => handleClick(slides.indexOf(slide))} className={`${active === slides.indexOf(slide) ? styles.currentButton : ''}`}></button>
+        })}
+      </nav>
     </section>
   );
 }
